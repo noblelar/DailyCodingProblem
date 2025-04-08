657437bedbfcdde65d42ded50c710656ebf41388
noblelar <59696875+noblelar@users.noreply.github.com>
2025-04-09 00:01:01 +0100

    bin

--- Solved/consec_run.py
+++ Solved/consec_run.py
@@ -1,9 +1,20 @@
 
 
+def get_binary(num):
+   binary = ""
+   while (num != 0):
+      remainder = num % 2
+      binary = str(remainder) + binary
+      num = (num - remainder) / 2
+
+   return binary
+
 def consec_run(num):
    consec_count = 0
    print(num)
 
+   num_in_binary = get_binary(num)
+   print(num_in_binary)
 
    return consec_run
 
