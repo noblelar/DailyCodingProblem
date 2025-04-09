349d2a8c1aa9d89603d9ca14a964ee09e8fbace9
noblelar <59696875+noblelar@users.noreply.github.com>
2025-04-09 23:26:26 +0100

    consec_run_done

--- Solved/consec_run.py
+++ Solved/consec_run.py
@@ -2,22 +2,33 @@
 
 def get_binary(num):
    binary = ""
+   abin = str(bin(num)[2:])
    while (num != 0):
-      remainder = num % 2
+      remainder = int(num % 2)
       binary = str(remainder) + binary
       num = (num - remainder) / 2
 
    return binary
 
+
 def consec_run(num):
    consec_count = 0
+   count = 0
    print(num)
 
    num_in_binary = get_binary(num)
-   print(num_in_binary)
+   print(len(num_in_binary))
+   for x in num_in_binary :
+      if x == "1":
+         count = count + 1
+         consec_count = count
+      else: 
+         count = 0
 
-   return consec_run
+
+   return consec_count
 
 
 
-consec_run(156)
+thevalue = consec_run(156)
+print(thevalue)
